0ef315f352e8672af478e29a7c5df1cd5052239c
ana-225 <ana-225@gmail.com>
2021-07-20 11:04:35 -0400

    Se modifica archivo package.json y se modifica  Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-=======
 import Navbranch from './component/Navbranch'
 import DropdownButtons from './component/DropdownButtons'
->>>>>>> ea0d901d4ac2ffb4122c767ab6ec1792a32e3922
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Navbranch/>
       <DropdownButtons/>
->>>>>>> ea0d901d4ac2ffb4122c767ab6ec1792a32e3922
     </div>
   );
 }
