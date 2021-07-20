3a581ce738f2753f7d791c4ee349a23e8a076990
Danipaz1 <dn.velasquezdevia@gmail.com>
2021-07-20 15:56:04 -0700

    login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbranch from './component/Navbranch'
 import DropdownButtons from './component/DropdownButtons'
+import SignInSide from './component/SingInSide';
 
 function App() {
   return (
     <div className="App">
+      <SignInSide/>
       <Navbranch/>
       <DropdownButtons/>
     </div>
