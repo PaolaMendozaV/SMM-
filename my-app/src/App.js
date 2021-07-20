ecd6f61e7db24b1654e0538b2ae0179904d10113
ana-225 <ana-225@gmail.com>
2021-07-20 15:57:43 -0400

    Creacion de route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbranch from './component/Navbranch'
-import DropdownButtons from './component/DropdownButtons'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbranch from "./component/Navbranch";
+import DropdownButtons from "./component/DropdownButtons";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbranch/>
-      <DropdownButtons/>
-    </div>
+    <Router>
+      <div className="App">
+    
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/start">
+            <Navbranch />
+            <DropdownButtons />
+          </Route>
+          <Route path="/reservation"></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
