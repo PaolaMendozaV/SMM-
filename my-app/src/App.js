dc930c5ac3c5478d2ea5bd1067d22f3853ef5323
PamePebess <pamepebess07@gmail.com>
2021-07-20 20:47:56 -0400

    botones y calendario

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbranch from './component/Navbranch'
 import DropdownButtons from './component/DropdownButtons'
+import ReservationButton from './component/ReservationButton';
+import Calendar from './component/Calendar';
 
 function App() {
   return (
     <div className="App">
       <Navbranch/>
       <DropdownButtons/>
+      <Calendar/>
+      <ReservationButton/>
     </div>
   );
 }
--- my-app/src/component/DropdownButtons.jsx
+++ my-app/src/component/DropdownButtons.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import {DropdownButton, Dropdown}from 'react-bootstrap';
+import React from "react";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 //import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const DropdownButtons = () => {
-    return (
-        <div>
-            <DropdownButton id="dropdown-item-button" title="Dropdown button">
-                <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
-                <Dropdown.Item as="button">Action</Dropdown.Item>
-                <Dropdown.Item as="button">Another action</Dropdown.Item>
-                <Dropdown.Item as="button">Something else</Dropdown.Item>
-            </DropdownButton>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <DropdownButton id="dropdown-item-button" title="Jornada">
+        <Dropdown.Item as="button">Diurno</Dropdown.Item>
+        <Dropdown.Item as="button">Vespertino</Dropdown.Item>
+        <Dropdown.Item as="button">Completa</Dropdown.Item>
+      </DropdownButton>
+      <DropdownButton id="dropdown-item-button" title="N° Persona">
+        <Dropdown.Item as="button">1</Dropdown.Item>
+        <Dropdown.Item as="button">2</Dropdown.Item>
+        <Dropdown.Item as="button">3</Dropdown.Item>
+        <Dropdown.Item as="button">4</Dropdown.Item>
+        <Dropdown.Item as="button">5</Dropdown.Item>
+        <Dropdown.Item as="button">6</Dropdown.Item>
+        <Dropdown.Item as="button">7</Dropdown.Item>
+        <Dropdown.Item as="button">8</Dropdown.Item>
+        <Dropdown.Item as="button">9</Dropdown.Item>
+        <Dropdown.Item as="button">10</Dropdown.Item>
+        <Dropdown.Item as="button">11</Dropdown.Item>
+        <Dropdown.Item as="button">12</Dropdown.Item>
+        <Dropdown.Item as="button">13</Dropdown.Item>
+        <Dropdown.Item as="button">14</Dropdown.Item>
+        <Dropdown.Item as="button">15</Dropdown.Item>
+    </DropdownButton>
+     </div>
+  );
+};
 
-export default DropdownButtons
+export default DropdownButtons;
